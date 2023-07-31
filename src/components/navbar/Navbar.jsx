5481cf75379bc02517a33affae60b0e2d81db253
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-08-01 00:13:37 +0530

    Navbar with responsive design (Mobile and Tablet Devices config)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,33 @@
 import classes from "./Navbar.module.css";
 import logo from "../../assets/logo.svg";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { useState } from "react";
+
+const Menu = () => {
+	return (
+		<>
+			<p>
+				<a href="#home">Home</a>
+			</p>
+			<p>
+				<a href="#gpt">What is GPT?</a>
+			</p>
+			<p>
+				<a href="#possibilty">Open AI</a>
+			</p>
+			<p>
+				<a href="#features">Case Studies</a>
+			</p>
+			<p>
+				<a href="#blog">Library</a>
+			</p>
+		</>
+	);
+};
 
 const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
+
 	return (
 		<div className={classes.navbar}>
 			<div className={classes.links}>
@@ -10,26 +35,40 @@ const Navbar = () => {
 					<img src={logo} alt="logo" />
 				</div>
 				<div className={classes.container}>
-					<p>
-						<a href="#home">Home</a>
-					</p>
-					<p>
-						<a href="#gpt">What is GPT?</a>
-					</p>
-					<p>
-						<a href="#possibilty">Open AI</a>
-					</p>
-					<p>
-						<a href="#features">Case Studies</a>
-					</p>
-					<p>
-						<a href="#blog">Library</a>
-					</p>
+					<Menu />
 				</div>
 			</div>
 			<div className={classes.signup}>
 				<p>Signin</p>
 				<button type="button">Signup</button>
+			</div>
+			<div className={classes.menu}>
+				{toggleMenu ? (
+					<RiCloseLine
+						color="#fff"
+						size={26}
+						onClick={() => setToggleMenu(false)}
+					/>
+				) : (
+					<RiMenu3Line
+						color="#fff"
+						size={26}
+						onClick={() => setToggleMenu(true)}
+					/>
+				)}
+				{toggleMenu && (
+					<div
+						className={`${classes.menu_container} ${classes.scale_up_center}`}
+					>
+						<div className={classes.mobile_menu_link}>
+							<Menu />
+						</div>
+						<div className={classes.mobile_signup}>
+							<p>Sign In</p>
+							<button type="button">Sign Up</button>
+						</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);

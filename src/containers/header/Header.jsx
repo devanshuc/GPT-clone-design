790586e4e9b546b23e79a8416a9a716a6dd2e1a7
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-08-01 01:03:06 +0530

    Initial Header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,36 @@
 import classes from "./Header.module.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className={`${classes.header} ${classes.section_padding}`} id="home">
+			<div className={classes.header_content}>
+				<h1 className="gradient__text">
+					Let’s Build Something amazing with GPT-3 OpenAI
+				</h1>
+				<p className="">
+					Yet bed any for travelling assistance indulgence unpleasing. Not
+					thoughts all exercise blessing. Indulgence way everything joy
+					alteration boisterous the attachment. Party we years to order allow
+					asked of.
+				</p>
+				<div className={classes.input}>
+					<input type="email" placeholder="Your Email Address"></input>
+					<button type="button">Get Started</button>
+				</div>
+
+				<div className={classes.people}>
+					<img src={people} alt="number of people" />
+					<p>1,600 people requested access a visit in last 24 hours</p>
+				</div>
+
+				{/* <div className={classes.ai_image}>
+					<img src={ai} alt="AI Image" />
+				</div> */}
+			</div>
+		</div>
+	);
 };
 
 export default Header;

1479f20a6bd66c799efd72bde4efa2c80fa9dc66
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-08-02 23:27:46 +0530

    GPT changes

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,7 +1,17 @@
 import classes from "./Features.module.css";
 
 const Features = () => {
-	return <div>Features</div>;
+	return (
+		<div className={`${classes.features} ${classes.section__padding}`}>
+			<div className={classes.features_heading}>
+				<h1 className="gradient__text">
+					The Future is Now and You Just Need to Realize It. Step into Future
+					Today. & Make it Happen.
+				</h1>
+				<p>Request Early Access to Get Started</p>
+			</div>
+		</div>
+	);
 };
 
 export default Features;

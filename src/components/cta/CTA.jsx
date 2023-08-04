f510de6845b20bfcf1ad43e3b72826d17ff7aa66
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-08-05 00:50:20 +0530

    CTA Init

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,7 +1,17 @@
 import classes from "./CTA.module.css";
 
 const CTA = () => {
-	return <div>CTA</div>;
+	return (
+		<div className={`${classes.cta} ${classes.section__margin}`}>
+			<div className={classes.cta_content}>
+				<p>Request Early Access to Get Started</p>
+				<h3>Register today & start exploring the endless possiblities.</h3>
+			</div>
+			<div className={classes.cta_button}>
+				<button>Get Started</button>
+			</div>
+		</div>
+	);
 };
 
 export default CTA;

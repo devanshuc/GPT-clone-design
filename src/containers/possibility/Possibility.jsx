7a09c5bd03ddb2cb7f44f113218ae8ffccb5c50e
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-08-04 23:59:42 +0530

    Few minor changes

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -3,28 +3,24 @@ import logo from "../../assets/possibility.png";
 
 const Possibility = () => {
 	return (
-		<div className={`${classes.possibility} ${classes.}`}>
-			<div className={classes.possibility_logo}>
+		<div className={`${classes.possibility} ${classes.section__padding}`}>
+			<div className={classes.possibility_image}>
 				<img src={logo} />
 			</div>
 			<div className={classes.container}>
-				<div className={classes.div1}>
-					<p>Request Early Access to Get Started</p>
-				</div>
-				<div className={classes.div2}>
-					<h1 className="gradient__text">
-						The possibilities are beyond your imagination
-					</h1>
-					<p>
-						Yet bed any for travelling assistance indulgence unpleasing. Not
-						thoughts all exercise blessing. Indulgence way everything joy
-						alteration boisterous the attachment. Party we years to order allow
-						asked of.
-					</p>
-				</div>
-				<div className={classes.div3}>
-					<p>Request Early Access to Get Started</p>
-				</div>
+				<p>Request Early Access to Get Started</p>
+
+				<h1 className="gradient__text">
+					The possibilities are beyond your imagination
+				</h1>
+				<p>
+					Yet bed any for travelling assistance indulgence unpleasing. Not
+					thoughts all exercise blessing. Indulgence way everything joy
+					alteration boisterous the attachment. Party we years to order allow
+					asked of.
+				</p>
+
+				<p>Request Early Access to Get Started</p>
 			</div>
 		</div>
 	);

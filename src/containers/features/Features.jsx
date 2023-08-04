d7ebc08cd8ea7fcccb1b0ea21375ce768fa6687e
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-08-04 18:12:15 +0530

    Features component (CSS remaining)

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,14 +1,35 @@
 import classes from "./Features.module.css";
+import { Feature } from "../../components";
 
 const Features = () => {
 	return (
-		<div className={`${classes.features} ${classes.section__padding}`}>
-			<div className={classes.features_heading}>
-				<h1 className="gradient__text">
-					The Future is Now and You Just Need to Realize It. Step into Future
-					Today. & Make it Happen.
-				</h1>
-				<p>Request Early Access to Get Started</p>
+		<div>
+			<div className={`${classes.features} ${classes.section__padding}`}>
+				<div className={classes.features_heading}>
+					<h1 className="gradient__text">
+						The Future is Now and You Just Need to Realize It. Step into Future
+						Today. & Make it Happen.
+					</h1>
+					<p>Request Early Access to Get Started</p>
+				</div>
+			</div>
+			<div>
+				<Feature
+					title="Improving end distrusts instantly"
+					text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+				/>
+				<Feature
+					title="Become the tended active"
+					text="Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."
+				/>
+				<Feature
+					title="Message or am nothing"
+					text="Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+				/>
+				<Feature
+					title="Really boy law county"
+					text="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."
+				/>
 			</div>
 		</div>
 	);

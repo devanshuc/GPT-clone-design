09fe4df5c6f2bbce2d33fdb9d86283b4de06b128
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-08-05 14:37:17 +0530

    Article and Blog added.

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,7 +1,21 @@
 import classes from "./Article.module.css";
 
-const Article = () => {
-	return <div className={classes.article}></div>;
+// eslint-disable-next-line react/prop-types
+const Article = ({ image }) => {
+	return (
+		<div className={classes.article}>
+			<div className={classes.image}>
+				<img src={image} alt="blog_image" />
+			</div>
+			<div className={classes.article_content}>
+				<div>
+					<p>Sept 26, 2021</p>
+					<h3>GPT-3 and Open AI is the future. Let us exlore how it is?</h3>
+				</div>
+				<p>Read Full Article</p>
+			</div>
+		</div>
+	);
 };
 
 export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,32 @@
-import classes from "./BLog.module.css";
+import { Article } from "../../components";
+import classes from "./Blog.module.css";
+import Blog1Img from "../../assets/blog01.png";
+import Blog2Img from "../../assets/blog02.png";
+import Blog3Img from "../../assets/blog03.png";
+import Blog4Img from "../../assets/blog04.png";
+import Blog5Img from "../../assets/blog05.png";
 
-const BLog = () => {
-	return <div>BLog</div>;
+const Blog = () => {
+	return (
+		<div className={`${classes.blog} ${classes.section__padding}`} id="blog">
+			<div className={classes.blog_heading}>
+				<h1 className="gradient__text">
+					A lot is happening, We are blogging about it.
+				</h1>
+			</div>
+			<div className={classes.blog_container}>
+				<div>
+					<Article image={Blog1Img} />
+				</div>
+				<div>
+					{/* <Article image={Blog2Img} />
+					<Article image={Blog3Img} />
+					<Article image={Blog4Img} />
+					<Article image={Blog5Img} /> */}
+				</div>
+			</div>
+		</div>
+	);
 };
 
-export default BLog;
+export default Blog;

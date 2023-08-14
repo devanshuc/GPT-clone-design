e43456d78bdb4f3a3f1ac10742d582e73bdb6bbf
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-08-14 12:05:40 +0530

    GPT Info Updated

--- src/containers/GPT/GPT.jsx
+++ src/containers/GPT/GPT.jsx
@@ -7,7 +7,7 @@ const Gpt = () => {
 			<div className={classes.feature}>
 				<Feature
 					title="What is GPT 3.5"
-					text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+					text="Imagine ChatGPT 3.5 as a super smart computer program that can understand and generate human-like text. It's like a virtual friend that knows a lot of things and can talk to you about almost anything. People use it to get information, help with writing, or just have fun conversations. It's like having a really clever robot buddy that you can chat with on the computer!"
 				/>
 			</div>
 			<div className={classes.heading}>
@@ -18,17 +18,16 @@ const Gpt = () => {
 			</div>
 			<div className={classes.container}>
 				<Feature
-					title="Chatbots"
-					text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+					title="Personalized Learning Assistant"
+					text="ChatGPT can help students learn by answering questions and explaining complex topics in a simple way. It's like having a smart tutor available 24/7, making studying more accessible and fun!"
 				/>
 				<Feature
-					title="Knowledgebase"
-					text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b
-"
+					title="Content Generation"
+					text="ChatGPT can assist writers, bloggers, and content creators by generating ideas, outlines, or even entire articles. It's like having a co-writer who can brainstorm with you and help you write faster."
 				/>
 				<Feature
-					title="Education"
-					text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+					title="Customer Support and FAQs"
+					text="ChatGPT can handle customer queries, provide instant support, and answer frequently asked questions on websites. It's like having a super-fast customer service agent that's available all the time, ensuring a great user experience."
 				/>
 			</div>
 		</div>
